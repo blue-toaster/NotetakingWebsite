2fbaae797037a49fa3d5bc44a48d1bef063cd868
SamM0mo <130717624+SamM0mo@users.noreply.github.com>
2024-07-26 10:03:47 +1000

    Add files via upload

---
+++ physics.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+    <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
+</head>
+<body>
+  <ul>
+    <li><a class="active" href="../index.php">Home</a></li>
+    <li><a href="physics.php">Physics</a></li>
+    <li><a href="biology.php">Biology</a></li>
+    <li><a href="chem.php">Chemistry</a></li>
+    <li><a href="cs.php">Computer Science</a></li>
+    <li><a href="enviro.php">Enviro</a></li>
+  </ul>
+    <h1>Physics: 2025+</h1>
+    <h2>Space, Time & Motion</h2>
+    <a href="physicsNotes\physics_stm_k.php">Kinematics</a><br>
+    <a href="physicsNotes\physics_stm_FaM.php">Forces & Momentum</a><br>
+    <a href="physicsNotes\physics_stm_wep.php">Work, Energy & Power</a><br>
+    <a href="physicsNotes\physics_stm_rbm.php">Rigidbody Mechanics</a><br>
+    <a href="physicsNotes\physics_stm_gsr.php">Galilean & Special Relativity</a><br>
+    <br>
+    <h2>The Particulate Nature of Matter</h2>
+    <a href="physicsNotes\physics_pnm_tet.php">Thermal Energy Transfers</a><br>
+    <a href="physicsNotes\physics_pnm_ge.php">Greenhouse Effect</a><br>
+    <a href="physicsNotes\physics_pnm_gl.php">Gas Laws</a><br>
+    <a href="physicsNotes\physics_pnm_thd.php">Thermodynamics</a><br>
+    <a href="physicsNotes\physics_pnm_cac.php">Currents & Circuits</a><br>
+    <br>
+    <h2>Wave Behaviour</h2>
+    <a href="physicsNotes\physics_wb_shm.php">Simple Harmonic Motion</a><br>
+    <a href="physicsNotes\physics_wb_wm.php">Wave Model</a><br>
+    <a href="physicsNotes\physics_wb_wp.php">Wave Phenomena</a><br>
+    <a href="physicsNotes\physics_wb_swr.php">Standing Waves & Resonance</a><br>
+    <a href="physicsNotes\physics_wb_de.php">Doppler Effect</a><br>
+    <br>
+    <h2>Fields</h2>
+    <a href="physicsNotes\physics_f_gf.php">Gravitational Fields</a><br>
+    <a href="physicsNotes\physics_f_emf.php">Electric & Magnetic Fields</a><br>
+    <a href="physicsNotes\physics_f_mef.php">Motion in Electromagnetic Fields</a><br>
+    <a href="physicsNotes\physics_f_i.php">Induction</a><br>
+    <br>
+    <h2>Nuclear & Quantum Physics</h2>
+    <a href="physicsNotes\physics_nqp_f.php">Standing Waves & Resonance</a><br>
+    <a href="physicsNotes\physics_nqp_fs.php">Doppler Effect</a><br>
+    <a href="physicsNotes\physics_nqp_sa.php">Simple Harmonic Motion</a><br>
+    <a href="physicsNotes\physics_nqp_qp.php">Wave Model</a><br>
+    <a href="physicsNotes\physics_nqp_rd.php">Wave Phenomena</a><br>
+
+</body>
+</html>
